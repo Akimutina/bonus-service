d4f9906f025e53f2ed2f1f97429468ef7f79beb9
Olga Akimutina <akimutina.olga@mail.ru>
2023-02-08 18:08:35 +0300

    Change commit

---
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.bonus;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class BonusServiceTest {
+
+    @ParameterizedTest
+
+    @CsvFileSource(files = "src/test/resources/data.csv")
+    public void testRegisteredUnderLimit(long amount, boolean registered, int expected) {
+        BonusService service = new BonusService();
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
